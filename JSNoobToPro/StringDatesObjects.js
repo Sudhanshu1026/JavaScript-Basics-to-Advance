60efe64de5275436f6d8d9fdc175a598971c4d42
Sudhanshu Singh <sudhanshusingh1026@gmail.com>
2026-07-21 23:45:50 +0530

    Dates Done

--- JSNoobToPro/StringDatesObjects.js
+++ JSNoobToPro/StringDatesObjects.js
@@ -49,3 +49,42 @@ console.log(c);
 
 //Date in Java script
 const now=new Date();
+
+console.log(now);
+console.log(now.toString());
+console.log(now.toISOString());
+console.log(now.toLocaleString());
+
+console.log(now.getDay());   // 1 Mon -7 Sun
+console.log(now.getDate());
+console.log(now.getFullYear());
+console.log(now.getMonth());    // 0 Jan-11 Feb
+
+
+
+
+//Inconsistencies in Date
+//Day:mon-tue(1 based)    Month:0-11(Jan-Dec)
+
+//Creating own date objects
+const now = new Date(2026,6,21,23,10,18,127);      //date(year,month,date,hour,min,sec,milisec)
+console.log(now);
+console.log(now.toString());
+//Returns 2026-07-21 -> 06 mon:August (0 Based) in input, But  07 mon:August(1 based) in output 
+
+
+
+
+
+//Millisecond Clock Starts/Timestamp in 1970
+const now=Date.now();
+console.log(now); //Output:1784656240935 current time always in milisecs
+const dates=new Date(1784656240935); //Curr milisec into dates and time
+console.log(dates);
+console.log(dates.toString());
+
+
+
+const firstDate=new Date(0); //milisec clock started from this Date : 8 Jan 1970
+console.log(firstDate.toString());
+
